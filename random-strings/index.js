ac46b0cf7669b3df5632e4c335392e45638a0e6e
Teemukoivumaa <35847478+Teemukoivumaa@users.noreply.github.com>
2022-06-19 12:44:19 +0300

    random-strings exe1.7

--- random-strings/index.js
+++ random-strings/index.js
@@ -1,12 +1,46 @@
+var express = require('express');
+let app = express();
+var port = process.env.PORT || 8080;
+
+var timestampedText = "";
 
 const randomText = () => {
-    let random = (Math.random() + 1).toString(36).substring(7);
+    let randomString = (Math.random() + 1).toString(36).substring(7);
+    let date = new Date();
     
-    console.log(random);
+    let timestamp = date.toISOString().slice(0, -5);
+    timestamp = timestamp.replace('T', ' ')
+    
+    timestampedText = `${timestamp} : ${randomString}`;
+    console.log(timestampedText);
 
-    setTimeout(randomText, 5000)
+    setTimeout(randomText, 5000);
 }
 
-randomText()
+app.get('/status', function(req, res) {
+    console.log("Getting status");
+    res.send
+        (
+        ` 
+        </br> </br>
+        <h2>${timestampedText}</h2>
+        `
+        )
+});
 
+app.get('/', function(req, res) {
+    console.log("Site pinged");
+    res.send
+        (
+        ` 
+        </br> </br>
+        <h2>Hello World!</h2>
+        `
+        )
+});
 
+app.listen(port, function () {
+    console.log("Server started in port: " + port);
+});
+
+randomText();
