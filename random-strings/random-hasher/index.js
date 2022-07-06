bdf533b144992f9520166726b4217038a60d7c71
Teemukoivumaa <35847478+Teemukoivumaa@users.noreply.github.com>
2022-07-06 19:22:24 +0300

    Exe 2.07 Add postgres database to random strings
    Add postgres database where the current ping-pong amount will be stored
    Merge random-hasher and timestamper together and remove the volumes

--- random-strings/random-hasher/index.js
+++ random-strings/random-hasher/index.js
@@ -5,13 +5,8 @@ const readLastLines = require('read-last-lines');
 const axios = require('axios')
 const router = express.Router();
 
-const dir = path.join('/', 'usr', 'src', 'app', 'files')
-//const dir = path.join('./')
-const filePath = path.join(dir, 'log.txt')
-
 let app = express();
 let port = process.env.PORT || 8080;
-app.use(express.static(dir));
 app.use(express.json());
 
 const pingPong = require('./pingPongs.js');
@@ -25,15 +20,13 @@ app.use('/pingPongs', pingPongRouter);
 var timestampedText = ""
 var pingpongText = ""
 
-const readFilesLastLine = (path) => {
-    return readLastLines.read(path, 1)
-        .then((line) => { return line });
-}
-
 async function randomText() {
     let randomString = (Math.random() + 1).toString(36).substring(7);
     
-    let timestamp = await readFilesLastLine(filePath)
+    let date = new Date()
+    let timestamp = date.toISOString().slice(0, -5)
+    timestamp = timestamp.replace('T', ' ')
+
     timestampedText = `${timestamp} : ${randomString}`;
     console.log(timestampedText);
     
