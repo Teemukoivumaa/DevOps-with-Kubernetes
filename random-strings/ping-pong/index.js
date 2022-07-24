415d0ef49d720901fc18e1b7a27c0fc7dffb5019
Teemukoivumaa <35847478+Teemukoivumaa@users.noreply.github.com>
2022-07-24 21:17:44 +0300

    Exe 4.01 Health checking random-strings
    Move deployments to their own files

--- random-strings/ping-pong/index.js
+++ random-strings/ping-pong/index.js
@@ -6,7 +6,7 @@ var port = process.env.PINGPONGPORT || 8085
 
 const password = process.env.POSTGRES_PASSWORD.toString()
 const { Client } = require('pg');
-const client = new Client({
+var client = new Client({
     host: 'postgres-svc',
     user: 'postgres',
     database: 'postgres',
@@ -35,25 +35,36 @@ async function queryDB(query) {
 }
 
 async function startDB() {
+    client = new Client({
+        host: 'postgres-svc',
+        user: 'postgres',
+        database: 'postgres',
+        password: password,
+        port: 5432
+    });
+    
     try {
         await client.connect()
         console.log("Connected to database")
     } catch(error) {
         console.log(error.stack)
-        console.log("Couldn't connect to database. Exiting...")
-        process.exit()
+        console.log("Couldn't connect to database")
+        return false
     }
     
     if (!queryDB(createTableQuery)) {
-        console.log("Couldn't create table to database. Exiting...")
-        process.exit()
+        console.log("Couldn't create table to database")
+        return false
     }
     console.log("Table was created")
 
     if(!queryDB(initialValue)) {
         console.log("Didn't insert initial values.")
+    } else {
+        console.log("Initial values added")
     }
-    console.log("Initial values added")
+
+    return true
 }
 
 const updateDB = () => {
@@ -75,7 +86,8 @@ const sendPingPongs = (pongs) => {
     });
 }
 
-app.get('/', function(req, res) {    
+app.get('/', function(req, res) {
+    console.log("New pong")
     client.query(countQuery, (error, response) => {
         if (error) throw error.stack
         console.log("Pongs:", response.rows[0].count)
@@ -90,12 +102,37 @@ app.get('/', function(req, res) {
 })
 
 app.post('/add', function(req, res) {
+    console.log("New pong request")
     updateDB()
+    res.status(200)
     res.send("Success!")
 })
 
+var healthy = false
 
-startDB()
+app.get('/check', async function(req, res) {
+    console.log("New health check")
+    if (healthy) {
+        res.status(200)
+        res.send("OK")
+        console.log("Health check OK")
+    } else {
+        var connectionToDB = await startDB()
+        console.log(`Connected to DB? ${connectionToDB}`)
+        if (connectionToDB) { 
+            res.status(200)
+            res.send("OK")
+            console.log("Health check OK")
+            healthy = true
+        }
+        else {
+            res.status(500)
+            res.send("Error")
+            console.log("Health check not OK")
+            healthy = false
+        }
+    }
+})
 
 app.listen(port, function () {
     console.log("Server started in port: " + port);
