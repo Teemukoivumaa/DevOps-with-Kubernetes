388a9c8f5a471df621908bd38886e6d238d57f5a
Teemukoivumaa <35847478+Teemukoivumaa@users.noreply.github.com>
2022-07-24 21:17:44 +0300

    Exe 4.01 Health checking random-strings

--- random-strings/random-hasher/index.js
+++ random-strings/random-hasher/index.js
@@ -62,6 +62,34 @@ app.get('/click', function(req, res) {
     })
 })
 
+var health = false
+
+app.get('/healthz', function(req, res) {
+    if (health) {
+        res.status(200)
+        res.send("OK")
+        console.log("Health check ping-pong connection is ok")
+    } else {
+        console.log("Health check ping-pong connection")
+    
+        axios.get('http://ping-pong-svc:2346/healthz')
+        .then(function (response) {
+            console.log(response)
+            health = true
+            console.log("Connection to ping-pong ok")
+            res.status(200)
+            res.send("OK")
+        })
+        .catch(function (error) {
+            health = false
+            console.log(error)
+            console.log("Connection to ping-pong failed")
+            res.status(500)
+            res.send("Error")
+        })
+    }
+})
+
 app.listen(port, function () {
     console.log("Server started in port: " + port);
 })
