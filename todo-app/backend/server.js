088aa376cb1d8fa953357b7f5a6bd1edd63a9d46
Teemu Koivumaa <35847478+Teemukoivumaa@users.noreply.github.com>
2022-07-25 20:14:22 +0300

    Exe 4.02 Add readiness checks and fix app to work

--- todo-app/backend/server.js
+++ todo-app/backend/server.js
@@ -14,6 +14,36 @@ let todoGET = todoBasics.getTodos
 app.use('/todos', todoPOST)
 app.use('/todos', todoGET)
 
+const database = require('./database.js')
+let startDB = database.startDB
+var healthy = false
+
+app.get('/check', async function(req, res) {
+    console.log("New health check")
+    if (healthy) {
+        res.status(200)
+        res.send("OK")
+        console.log("Health check OK")
+    } else {
+        var connectionToDB = await startDB()
+        console.log(`Connected to DB? ${connectionToDB}`)
+        if (connectionToDB) {
+            res.status(200)
+            res.send("OK")
+            console.log("Health check OK")
+            healthy = true
+        }
+        else {
+            res.status(500)
+            res.send("Error")
+            console.log("Health check not OK")
+            healthy = false
+        }
+    }
+    console.log("")
+})
+
+
 app.listen(port, function () {
     console.log("TODO-backend started in port: " + port);
 })
