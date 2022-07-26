97474a40bc225fcc806b4d0133f820c720393a73
Teemukoivumaa <35847478+Teemukoivumaa@users.noreply.github.com>
2022-07-26 19:18:29 +0300

    Exe 4.05 Add ability to mark todos as done

--- todo-app/frontend/index.js
+++ todo-app/frontend/index.js
@@ -9,6 +9,17 @@ async function getTodos() {
     }
 }
 
+async function getDoneTodos() {
+    const response = await fetch('/getTodos/done', {
+        method: 'GET',
+        mode: 'no-cors',
+    })
+
+    if (response.status == "200") {
+        return response.json()
+    }
+}
+
 async function addTodo(newTodo) {
     const response = await fetch('/newTodo', {
         method: 'POST',
@@ -22,19 +33,51 @@ async function addTodo(newTodo) {
     } else createTodoList()
 }
 
+async function addAsDone(id) {
+    const response = await fetch('/updateTodo', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({todoID:`${id}`})
+    })
+
+    if (response.status != "200") {
+        console.log(response)
+    } else createTodoList()
+}
+
+var undoneTodos = []
+var doneTodos = []
+
 async function createTodoList() {
     let todos = await getTodos()
-    
     if (todos) {
+        undoneTodos = todos
         var todoList = document.getElementById("todo-list");
         todoList.innerHTML = ""
         
         var list = "<ul>"
         for (let todo of todos) {
-            list = list.concat(`\n<li>${todo}</li>`)
+            list = list.concat(`\n<li id='${todo.id}' onClick='addAsDone(${todo.id})' >${todo.todo}</li>`)
         }
         list.concat("</ul>")
         
+        todoList.innerHTML = list
+    }
+    
+    let todosDone = await getDoneTodos()
+    
+    if (todosDone) {
+        doneTodos = todosDone
+        var todoList = document.getElementById("doneTodo-list");
+        todoList.innerHTML = ""
+
+        var list = "<ul>"
+        for (let todo of todosDone) {
+            list = list.concat(`\n<li><del>${todo.todo}</del></li>`)
+        }
+        list.concat("</ul>")
+
         todoList.innerHTML = list
     }
 }
